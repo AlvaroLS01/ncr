3114343a0b41bad66b5d683375e4a210969e209b
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-10-03 12:12:32 +0200

    Refrescar líneas de artículos tras promociones de fidelización

--- src/main/java/com/comerzzia/ametller/pos/ncr/actions/sale/AmetllerItemsManager.java
+++ src/main/java/com/comerzzia/ametller/pos/ncr/actions/sale/AmetllerItemsManager.java
@@ -1,6 +1,12 @@
 package com.comerzzia.ametller.pos.ncr.actions.sale;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
 
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Lazy;
@@ -9,11 +15,17 @@ import org.springframework.stereotype.Service;
 
 import com.comerzzia.ametller.pos.ncr.ticket.AmetllerScoTicketManager;
 import com.comerzzia.pos.ncr.actions.sale.ItemsManager;
+import com.comerzzia.pos.ncr.messages.Coupon;
 import com.comerzzia.pos.ncr.messages.ItemException;
 import com.comerzzia.pos.ncr.messages.ItemSold;
+import com.comerzzia.pos.ncr.messages.ItemVoided;
+import com.comerzzia.pos.ncr.messages.VoidItem;
 import com.comerzzia.pos.services.ticket.lineas.LineaTicket;
+import com.comerzzia.pos.services.ticket.TicketVentaAbono;
 import com.comerzzia.pos.util.bigdecimal.BigDecimalUtil;
 import com.comerzzia.pos.util.i18n.I18N;
+import com.comerzzia.pos.services.ticket.promociones.IPromocionTicket;
+import com.comerzzia.pos.services.ticket.promociones.PromocionTicket;
 
 @Lazy(false)
 @Service
@@ -60,10 +72,140 @@ public class AmetllerItemsManager extends ItemsManager {
 
         return itemSold;
     }
-    
-	@Override
-	public boolean isCoupon(String code) {
-		boolean couponAlreadyApplied = globalDiscounts.containsKey(GLOBAL_DISCOUNT_COUPON_PREFIX + code);
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void updateItems() {
+        ticketManager.getSesion().getSesionPromociones().aplicarPromociones((TicketVentaAbono) ticketManager.getTicket());
+        ticketManager.getTicket().getTotales().recalcular();
+
+        Map<String, HeaderDiscountData> headerDiscounts = getHeaderDiscountsDetail();
+
+        for (HeaderDiscountData headerDiscountData : headerDiscounts.values()) {
+            GlobalDiscountData globalDiscountData = globalDiscounts.get(GLOBAL_DISCOUNT_PAYMENT_PREFIX + headerDiscountData.paymentMethodCode);
+
+            if (globalDiscountData == null) {
+                globalDiscountData = new GlobalDiscountData();
+                globalDiscountData.headerDiscount = headerDiscountData;
+
+                ItemSold discount = new ItemSold();
+                discount.setFieldValue(ItemSold.ItemNumber, String.valueOf(globalDiscountCounter));
+                discount.setFieldIntValue(ItemSold.DiscountAmount, headerDiscountData.amount);
+                discount.setFieldValue(ItemSold.RewardLocation, "1");
+                discount.setFieldValue(ItemSold.ShowRewardPoints, "1");
+                discount.setFieldValue(ItemSold.DiscountDescription, headerDiscountData.paymentMethodDes);
+
+                globalDiscountCounter++;
+
+                globalDiscountData.itemSoldMessage = discount;
+
+                globalDiscounts.put(GLOBAL_DISCOUNT_PAYMENT_PREFIX + headerDiscountData.paymentMethodCode, globalDiscountData);
+
+                ncrController.sendMessage(discount);
+
+                sendTotals();
+            } else {
+                ItemSold discount = globalDiscountData.itemSoldMessage;
+
+                if (headerDiscountData.amount.compareTo(discount.getFieldBigDecimalValue(ItemSold.DiscountAmount, 2)) != 0) {
+                    log.debug("Updating header discount " + discount.getFieldValue(ItemSold.ItemNumber));
+
+                    discount.setFieldIntValue(ItemSold.DiscountAmount, headerDiscountData.amount);
+
+                    ncrController.sendMessage(discount);
+
+                    sendTotals();
+                }
+            }
+        }
+
+        if (globalDiscounts.size() > 0) {
+            Iterator<Entry<String, GlobalDiscountData>> globalDiscountIterator = globalDiscounts.entrySet().iterator();
+
+            while (globalDiscountIterator.hasNext()) {
+                Entry<String, GlobalDiscountData> grobalDiscountItem = globalDiscountIterator.next();
+
+                GlobalDiscountData globalDiscountData = grobalDiscountItem.getValue();
+
+                if (globalDiscountData.coupon != null) {
+                    boolean applied = false;
+
+                    for (PromocionTicket appliedPromotion : ((TicketVentaAbono) ticketManager.getTicket()).getPromociones()) {
+                        if (IPromocionTicket.COUPON_ACCESS.equals(appliedPromotion.getAcceso()) &&
+                                StringUtils.equals(appliedPromotion.getCodAcceso(), globalDiscountData.coupon.getCouponCode())) {
+                            applied = true;
+                            break;
+                        }
+                    }
+
+                    if (!applied) {
+                        ItemVoided voidItem = new ItemVoided();
+                        voidItem.setFieldValue(VoidItem.ItemNumber, globalDiscountData.couponMessage.getFieldValue(Coupon.ItemNumber));
+                        ncrController.sendMessage(voidItem);
+
+                        sendTotals();
+
+                        globalDiscountIterator.remove();
+                    }
+                } else if (globalDiscountData.headerDiscount != null) {
+                    if (!headerDiscounts.containsKey(globalDiscountData.headerDiscount.paymentMethodCode)) {
+                        ItemVoided voidItem = new ItemVoided();
+                        voidItem.setFieldValue(VoidItem.ItemNumber, globalDiscountData.itemSoldMessage.getFieldValue(ItemSold.ItemNumber));
+                        ncrController.sendMessage(voidItem);
+
+                        sendTotals();
+
+                        globalDiscountIterator.remove();
+                    }
+                }
+
+            }
+        }
+
+        for (LineaTicket ticketLine : (List<LineaTicket>) ticketManager.getTicket().getLineas()) {
+            ItemSold currentItemSold = linesCache.get(ticketLine.getIdLinea());
+
+            if (currentItemSold != null) {
+                ItemSold newItemSold = lineaTicketToItemSold(ticketLine);
+
+                String actualDiscount = currentItemSold.getDiscountApplied().getFieldValue(ItemSold.DiscountAmount);
+                String newDiscount = newItemSold.getDiscountApplied().getFieldValue(ItemSold.DiscountAmount);
+
+                if (!StringUtils.equals(actualDiscount, newDiscount)) {
+                    log.debug("Updating line " + ticketLine.getIdLinea());
+
+                    if (!StringUtils.equals(actualDiscount, "0") && StringUtils.equals(newDiscount, "0")) {
+                        ItemVoided voidItem = new ItemVoided();
+                        voidItem.setFieldValue(VoidItem.ItemNumber, currentItemSold.getDiscountApplied().getFieldValue(ItemSold.ItemNumber));
+                        ncrController.sendMessage(voidItem);
+
+                        ncrController.sendMessage(newItemSold);
+                        sendTotals();
+                    } else if (!StringUtils.equals(newDiscount, "0")) {
+                        ItemSold discountUpdate = newItemSold.getDiscountApplied();
+                        String discountPrice = discountUpdate.getFieldValue(ItemSold.Price);
+
+                        if (StringUtils.isNotBlank(discountPrice)) {
+                            discountUpdate.setFieldValue(ItemSold.ExtendedPrice, discountPrice);
+                        }
+
+                        ncrController.sendMessage(newItemSold);
+                        ncrController.sendMessage(discountUpdate);
+                        sendTotals();
+                    } else {
+                        ncrController.sendMessage(newItemSold);
+                        sendTotals();
+                    }
+
+                    linesCache.put(ticketLine.getIdLinea(), newItemSold);
+                }
+            }
+        }
+    }
+
+        @Override
+        public boolean isCoupon(String code) {
+                boolean couponAlreadyApplied = globalDiscounts.containsKey(GLOBAL_DISCOUNT_COUPON_PREFIX + code);
 
 		boolean handled = super.isCoupon(code);
 
